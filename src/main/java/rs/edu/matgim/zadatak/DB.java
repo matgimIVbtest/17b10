969d3e7131e8b567091a9f07ebd879a3c99df600
Lana <Lana@DELL>
2021-04-15 10:02:22 +0200

    zavrsen kontrolni!

--- src/main/java/rs/edu/matgim/zadatak/DB.java
+++ src/main/java/rs/edu/matgim/zadatak/DB.java
@@ -4,8 +4,11 @@ import java.sql.*;
 
 public class DB {
 
+    //Connection connection;
     String connectionString = "jdbc:sqlite:src\\main\\java\\KompanijaZaPrevoz.db";
-
+    
+    
+    //print firma
     public void printFirma() {
         try (Connection conn = DriverManager.getConnection(connectionString); Statement s = conn.createStatement()) {
 
@@ -25,5 +28,106 @@ public class DB {
             System.out.println(ex);
         }
     }
+    
+    //ukupna duzina
+    public void UkupnaDuzina(){
+         try (Connection conn = DriverManager.getConnection(connectionString); Statement s = conn.createStatement()) 
+        {
+            String upit = "SELECT Z.IDZap,Z.ImePrezime,SUM(Duzina) FROM Vozi V, Zaposlen Z, Putovanje P WHERE Z.IDZap=V.IDZap AND V.IDPut=P.IDPut GROUP BY Z.IDZap,Z.ImePrezime";
+            ResultSet rs = s.executeQuery(upit);
+            while (rs.next()) {
+                int IdZap = rs.getInt(1);
+                String Naziv = rs.getString(2);
+                int UkupnaDuzina = rs.getInt(3);
+
+                System.out.println(String.format("%d  %20s  %d\n", IdZap, Naziv, UkupnaDuzina));
+            }
+            
+
+        } catch (SQLException ex) {
+            System.out.println("Greska u ukupnoj duzini");
+            System.out.println(ex);
+        }
+    }
+    
+    //zaposli vozaca
+    public int Zadatak(String imeIPrezime, String Kategorija)
+    {
+        try (Connection conn = DriverManager.getConnection(connectionString); Statement s = conn.createStatement()) 
+        {
+            //nalazim id vozaca
+            String u1 = "SELECT MAX(IDZap)+1 FROM Zaposlen";
+            ResultSet rs = s.executeQuery(u1);
+            int IdZap = rs.getInt(1);
+            
+            //unosim vozaca u tabelu Zaposlen
+            String UnesiVozaca = "INSERT INTO Zaposlen (IdZap,ImePrezime,Staz) VALUES(?,?,0)";
+            PreparedStatement st1 = conn.prepareStatement(UnesiVozaca);
+            st1.setInt(1,IdZap);
+            st1.setString(2,imeIPrezime);
+            st1.executeUpdate();
+
+            //unosim vozaca u tabelu Vozac
+            String UnesiKategoriju = "INSERT INTO Vozac (Kategorija,IdZap) VALUES(?,?)";
+            PreparedStatement st2 = conn.prepareStatement(UnesiKategoriju);
+            st2.setString(1,Kategorija);
+            st2.setInt(2, IdZap);
+            st2.executeUpdate();
+            
+            //provera koliko ima najavljenih putovanja
+            String u2 = "SELECT COUNT(IDPut) FROM Putovanje WHERE Status='N'";
+            ResultSet rs2 = s.executeQuery(u2);
+            int BrojNajavljenih = rs2.getInt(1);
+            //System.out.println(BrojNajavljenih); //neka provera
+            
+            //ukoliko nema najavljenih putovanja
+            if(BrojNajavljenih==0)
+            {
+                return -1;
+            }
+            
+            else
+            {
+                String u3 = "SELECT COUNT(DISTINCT P.IDPut) FROM Putovanje P, Vozi V WHERE P.IDPut = V.IDPut and P.Status='N'";
+                ResultSet rs3=s.executeQuery(u3);
+                int BrojDodeljenih = rs3.getInt(1);
+                
+                //ako su sva putovanja vec dodeljena
+                if(BrojDodeljenih == BrojNajavljenih)
+                {
+                    String minIdPut = "SELECT MIN(IDPut) FROM Putovanje WHERE Status='N'";
+                    ResultSet min=s.executeQuery(minIdPut);
+                    int IdPut=min.getInt(1);
+                    
+                    String dodelaPutovanja="INSERT INTO Vozi (IDZap,IDPut) VALUES(?,?)";
+                    PreparedStatement stDodela = conn.prepareStatement(dodelaPutovanja);
+                    stDodela.setInt(1,IdZap);
+                    stDodela.setInt(2, IdPut);                    
+                }
+                
+                //ako postoji neko putovanje koje nije dodeljeno
+                else
+                {
+                    String stringic = "SELECT MIN(P.IDPut) FROM Putovanje P WHERE P.Status='N' EXCEPT SELECT V.IDPut FROM  Vozi V";
+                    ResultSet rsrs=s.executeQuery(stringic);
+                    int IdPut1=rsrs.getInt(1);
+                    
+                    String dodelaPutovanja1="INSERT INTO Vozi (IDZap,IDPut) VALUES(?,?)";
+                    PreparedStatement stDodela1 = conn.prepareStatement(dodelaPutovanja1);
+                    stDodela1.setInt(1,IdZap);
+                    stDodela1.setInt(2, IdPut1);
+                }
+                
+                System.out.println("Uspesna realizacija!");
+                return 1;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Dogodila se greska.");
+            System.out.println(ex);
+        }
+        return 1;
+    }
+
 
 }
--- src/main/java/rs/edu/matgim/zadatak/Program.java
+++ src/main/java/rs/edu/matgim/zadatak/Program.java
@@ -1,11 +1,22 @@
 package rs.edu.matgim.zadatak;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
 
         DB _db = new DB();
-        _db.printFirma();
+        _db.UkupnaDuzina();
+        
+        System.out.println();
+        
+        //_db.Zadatak("Petar","B");
+        
+        Scanner sc= new Scanner(System.in); 
+        String ime = sc.nextLine();
+        String kategorija = sc.nextLine();
+        _db.Zadatak(ime,kategorija);
         
     }
 }
